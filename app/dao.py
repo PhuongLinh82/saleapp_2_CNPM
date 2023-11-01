e266d444d125a58eed2ab08602ee378b6cbd934e
2151013049linh@ou.edu.vn <2151013049linh@ou.edu.vn>
2023-11-01 14:55:57 +0700

    push the next part of project app on third day

--- app/dao.py
+++ app/dao.py
@@ -1,35 +1,14 @@
-def get_categories():
-    return [{
-        "id": 1,
-        "name": "Mobile"
-    }, {
-        "id": 2,
-        "name": "Tablet"
-    }]
+from app.models import Category, Product
 
-def get_products(kw):
-    products = [{
-        "id": 1,
-        "name": "iPhone 15",
-        "price": 20000000,
-        "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg"
-    }, {
-        "id": 2,
-        "name": "Galaxy A12",
-        "price": 20000000,
-        "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg"
-    }, {
-        "id": 3,
-        "name": "iPhone 14",
-        "price": 20000000,
-        "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg"
-    }, {
-        "id": 4,
-        "name": "iPhone 13",
-        "price": 20000000,
-        "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg"
-    }]
+
+def load_categories():
+    return Category.query.all()
+
+
+def load_products(kw=None):
+    products = Product.query
 
     if kw:
-        products = [p for p in products if p['name'].find(kw) >= 0]
-    return products
+        products = products.filter(Product.name.contains(kw))
+    return products.all()
+
--- app/index.py
+++ app/index.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template, request
+from flask import render_template, request
 import dao
+from app import app
 
-app = Flask(__name__)
 
 @app.route("/")
 def index():
     kw = request.args.get('kw')
-    cates = dao.get_categories()
-    prods = dao.get_products(kw)
+    cates = dao.load_categories()
+    prods = dao.load_products(kw)
     return render_template('index.html', categories=cates, products=prods)
 
 if __name__ == '__main__':
